274329a00ff9e28498652865e1425630fc933188
jahidul9957 <142164832+jahidul9957@users.noreply.github.com>
2026-04-14 09:33:12 +0530

    Add URL path for add_funds view

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('add-funds/', views.add_funds, name='add_funds'),
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('services/', views.services, name='services'),
