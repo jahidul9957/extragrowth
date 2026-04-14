a27dd19b961f58fe7c6fbfb0b4e1ae3da72f5039
jahidul9957 <142164832+jahidul9957@users.noreply.github.com>
2026-04-14 06:40:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+# Apna superuser banane ka code
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=adminpass
+export DJANGO_SUPERUSER_EMAIL=admin@admin.com
+python manage.py createsuperuser --noinput || true
