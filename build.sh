0c511a4e11afd0c36a99a456734de275d3b55cb1
jahidul9957 <142164832+jahidul9957@users.noreply.github.com>
2026-04-14 08:39:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# 1. Zaroori packages install karein
+# 1. Packages install karein
 pip install -r requirements.txt
 
-# 2. Database ki tables banayein (Yeh sabse zaroori hai!)
+# 2. Database tables banayein (Yahan 'core' likhna zaroori hai)
+python manage.py makemigrations core
 python manage.py makemigrations
 python manage.py migrate
 
-# 3. Apna Admin account banayein
+# 3. Admin account banayein
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=adminpass
 export DJANGO_SUPERUSER_EMAIL=admin@admin.com
