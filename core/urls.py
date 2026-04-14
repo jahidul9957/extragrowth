4401672dbd1397e8c7fb26d6901f455708c0cf3e
jahidul9957 <142164832+jahidul9957@users.noreply.github.com>
2026-04-14 10:15:50 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'), # (Aapke purane links...)
+    path('login/', views.login_view, name='login'),
     path('services/', views.services, name='services'),
     path('team/', views.team, name='team'),
     path('profile/', views.profile, name='profile'),
     
-    # 👇 bas yeh ek line yahan jod dein:
+    # 🚀 NextGen Ai Dev - Naye Links
     path('add-funds/', views.add_funds, name='add_funds'),
     path('new-order/', views.new_order, name='new_order'),
-    
 ]
